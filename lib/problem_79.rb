37b7b86b8eab440d64d7f07fec401e181fb509b9
Tim Rand <timrandg@gmail.com>
2011-11-05 23:31:31 -0700

    adding Parse#new, parse.rows, parse.columns

--- lib/problem_79.rb
+++ lib/problem_79.rb
@@ -1,12 +1,21 @@
-#Problem 79
-#17 September 2004
-#
-#A common security method used for online banking is to ask the user for three random characters from a passcode. For example, if the passcode was 531278, they may ask for the 2nd, 3rd, and 5th characters; the expected reply would be: 317.
-#
-#The text file, keylog.txt, contains fifty successful login attempts.
-#
-#Given that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length.
-#
+
 
 class Problem_79
 end    
+
+class Parse
+  def initialize(file)
+    @num_file = file
+  end
+  def rows
+    lines = File.readlines(@num_file)
+    lines = lines.compact.uniq
+    lines = lines.select{|line| line.match(/^\d+$/)}
+    lines = lines.collect{|line| line.chomp.split('')}
+    lines = lines.collect{|arr| arr.map(&:to_i)}
+  end
+  def columns
+    columns = rows.transpose
+  end
+end
+
--- test/test_problem_79.rb
+++ test/test_problem_79.rb
@@ -5,7 +5,12 @@ Dir.glob(File.dirname(__FILE__) + '/../lib/*'){|file| require file}
 require "test/unit"
 
 class TestProblem_79 < Test::Unit::TestCase
-  def test_hello_from_tests
-    puts "hello from tests"
+  def test_can_parse_num_file_into_rows
+    data = Parse.new("../num_list.txt")
+    assert_equal [[1,2,3], [1,4,3], [1,2,4], [2,3,4]], data.rows
+  end
+  def test_can_parse_num_file_into_columns
+    data = Parse.new("../num_list.txt")
+    assert_equal [[1,1,1,2], [2,4,2,3], [3,3,4,4]], data.columns
   end
 end
